df914ca0161a2d9c2d1089934328fb9de77a2b43
syf <2654525303@qq.com>
2025-04-21 22:06:59 +0800

    snapping back card on right click

---
+++ src/Game/Scripts/CardUI/Hand.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+namespace CardGameV1.CardUI;
+
+public partial class Hand : HBoxContainer
+{
+    public override void _Ready()
+    {
+        foreach (var child in GetChildren())
+        {
+            if (child is CardUI cardUI)
+            {
+                cardUI.ReparentRequested += OnCardUIReparentRequested;
+            }
+        }
+    }
+
+    private void OnCardUIReparentRequested(CardUI cardUI) => cardUI.Reparent(this);
+}
