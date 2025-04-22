4de2fd17a2215d47849a64441b922861b180818e
syf <2654525303@qq.com>
2025-04-22 11:13:54 +0800

    add card resource

---
+++ src/Game/Scripts/CustomResources/Card.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+
+namespace CardGameV1.CustomResources;
+
+[GlobalClass]
+public partial class Card : Resource
+{
+    [ExportGroup("Card Attributes")]
+    [Export]
+    private string id = "default";
+
+    [Export] private CardType type;
+
+    [Export] private TargetType target;
+
+    public bool IsSingleTargeted => target == TargetType.SingleEnemy;
+
+    #region card enums
+
+    public enum CardType
+    {
+        Attack,
+        Skill,
+        Power
+    }
+
+    public enum TargetType
+    {
+        Self,
+        SingleEnemy,
+        AllEnemies,
+        Everyone
+    }
+
+    #endregion
+}
