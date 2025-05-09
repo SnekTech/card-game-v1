6cd40230f4f80226aca8f9f4f1522293ab54e1ea
syf <2654525303@qq.com>
2025-05-09 22:17:15 +0800

    create stats UI scene
    - add missing uid for AimingState.cs and CardEventBus.cs

---
+++ src/Game/Scripts/UI/StatsUI.cs
@@ -0,0 +1,40 @@
+﻿using CardGameV1.CustomResources;
+using Godot;
+using GodotUtilities;
+
+namespace CardGameV1.UI;
+
+[Scene]
+public partial class StatsUI : HBoxContainer
+{
+    [Node]
+    private HBoxContainer blockContainer = null!;
+
+    [Node]
+    private HBoxContainer healthContainer = null!;
+
+    [Node]
+    private Label blockLabel = null!;
+
+    [Node]
+    private Label healthLabel = null!;
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+
+    public void UpdateStats(Stats stats)
+    {
+        var (block, health) = (stats.Block, stats.Health);
+
+        blockLabel.Text = block.ToString();
+        healthLabel.Text = health.ToString();
+
+        blockLabel.Visible = block > 0;
+        healthLabel.Visible = health > 0;
+    }
+}
