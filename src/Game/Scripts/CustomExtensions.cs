67c625f61ca7c96b7424c6ffde282ed2b7d2ead8
syf <2654525303@qq.com>
2025-05-10 17:39:14 +0800

    add some custom extensions
    - Task fire & forget
    - delay using Godot temporary timer

---
+++ src/Game/Scripts/CustomExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Godot;
+
+namespace CardGameV1;
+
+public static class CustomExtensions
+{
+    public static async void Fire(this Task task, Action? onComplete = null, Action<Exception>? onError = null)
+    {
+        try
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception e)
+            {
+                GD.Print("something wrong during fire & forget: ");
+                GD.Print(e);
+                onError?.Invoke(e);
+            }
+
+            onComplete?.Invoke();
+        }
+        catch (Exception e)
+        {
+            GD.Print("something wrong on fire & forget complete : ");
+            GD.Print(e);
+            onError?.Invoke(e);
+        }
+    }
+
+    public static async Task DelayGd(this Node node, double timeSec, bool processAlways = true, bool processInPhysics = false, bool ignoreTimeScale = false)
+    {
+        var timer = node.GetTree().CreateTimer(timeSec, processAlways, processInPhysics, ignoreTimeScale);
+        await timer.ToSignal(timer, Timer.SignalName.Timeout);
+    }
+}
