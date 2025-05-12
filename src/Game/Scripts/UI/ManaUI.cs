6fc1ce0f72795b47d4494b723691aa965ac1640e
syf <2654525303@qq.com>
2025-05-12 11:54:47 +0800

    create the mana UI scene

---
+++ src/Game/Scripts/UI/ManaUI.cs
@@ -0,0 +1,57 @@
+﻿using CardGameV1.CustomResources;
+using Godot;
+using GodotUtilities;
+
+namespace CardGameV1.UI;
+
+[Scene]
+public partial class ManaUI : Panel
+{
+    [Export]
+    private CharacterStats originalStats = null!;
+
+    [Node]
+    private Label manaLabel = null!;
+
+    private bool _hasSubscribedStatsChanged;
+    private CharacterStats _stats = null!;
+
+    public CharacterStats Stats
+    {
+        get => _stats;
+        set
+        {
+            _stats = value;
+            SubscribeStatsChanged();
+        }
+    }
+
+    public override void _Ready()
+    {
+        Stats = originalStats;
+    }
+
+    private void SubscribeStatsChanged()
+    {
+        if (_hasSubscribedStatsChanged == false)
+        {
+            Stats.StatsChanged += UpdateManaLabel;
+        }
+
+        _hasSubscribedStatsChanged = true;
+    }
+
+    private void UpdateManaLabel()
+    {
+        var (mana, maxMana) = (Stats.Mana, Stats.MaxMana);
+        manaLabel.Text = $"{mana}/{maxMana}";
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+}
