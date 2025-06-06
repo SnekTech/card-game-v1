09281cd49dff37278d98f141f0fe3b61b02946a4
syf <2654525303@qq.com>
2025-06-06 20:24:11 +0800

    create the CardPileOpener scene

---
+++ src/Game/Scripts/UI/CardPileView/CardPileOpener.cs
@@ -0,0 +1,40 @@
+﻿using CardGameV1.CustomResources;
+using Godot;
+
+namespace CardGameV1.UI.CardPileView;
+
+public partial class CardPileOpener : TextureButton
+{
+    [Export]
+    private Label counter = null!;
+
+    private CardPile? _cardPile;
+
+    public CardPile CardPile
+    {
+        set
+        {
+            if (_cardPile != null)
+            {
+                _cardPile.CardPileSizeChanged -= OnCardPileSizeChanged;
+            }
+
+            _cardPile = value;
+            _cardPile.CardPileSizeChanged += OnCardPileSizeChanged;
+
+            UpdateCounter(_cardPile.Cards.Count);
+        }
+    }
+
+    public override void _ExitTree()
+    {
+        if (_cardPile != null)
+        {
+            _cardPile.CardPileSizeChanged -= OnCardPileSizeChanged;
+        }
+    }
+
+    private void UpdateCounter(int count) => counter.Text = count.ToString();
+
+    private void OnCardPileSizeChanged(int cardsAmount) => UpdateCounter(cardsAmount);
+}
