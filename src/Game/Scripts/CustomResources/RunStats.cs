a8123d4ccfbd5fe52d34e485d04096bb43f34629
syf <2654525303@qq.com>
2025-06-09 16:23:45 +0800

    update RunStats to get ready for CardRewards feature

--- src/Game/Scripts/CustomResources/RunStats.cs
+++ src/Game/Scripts/CustomResources/RunStats.cs
@@ -9,6 +9,10 @@ public partial class RunStats : Resource
     public event Action? GoldChanged;
 
     private const int StartingGold = 70;
+    private const int BaseCardRewards = 3;
+    private const float BaseCommonWeight = 6f;
+    private const float BaseUncommonWeight = 3.7f;
+    private const float BaseRareWeight = 0.3f;
 
     private int _gold = StartingGold;
 
@@ -20,5 +24,17 @@ public partial class RunStats : Resource
             _gold = value;
             GoldChanged?.Invoke();
         }
+    }
+
+    public int CardRewards { get; set; } = BaseCardRewards;
+    public float CommonWeight { get; set; } = BaseCommonWeight;
+    public float UncommonWeight { get; set; } = BaseUncommonWeight;
+    public float RareWeight { get; set; } = BaseRareWeight;
+
+    public void ResetWeights()
+    {
+        CommonWeight = BaseCommonWeight;
+        UncommonWeight = BaseUncommonWeight;
+        RareWeight = BaseRareWeight;
     }
 }
