0f35197aa20fe8c4ba93fc3e3b354ec14d343f70
syf <2654525303@qq.com>
2025-06-09 10:54:27 +0800

    add new warrior cards

---
+++ src/Game/Scripts/CustomResources/Cards/Warrior/WarriorBigSlam.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CardGameV1.EffectSystem;
+using Godot;
+
+namespace CardGameV1.CustomResources.Cards.Warrior;
+
+[GlobalClass]
+public partial class WarriorBigSlam : Card
+{
+    private const int DamageAmount = 10;
+
+    protected override CardAttributes Attributes { get; } = new()
+    {
+        Id = nameof(WarriorBigSlam),
+        Cost = 2,
+        Type = CardType.Attack,
+        Rarity = CardRarity.Uncommon,
+        Target = CardTarget.SingleEnemy,
+        TooltipText = $"[center]Deal [color=\"ff0000\"]{DamageAmount}[/color] damage and apply 2 [color=\"ffdf00\"] Exposed[/color].[/center]",
+        IconPath = "res://art/tile_0117.png",
+        SoundPath = "res://art/slash.ogg"
+    };
+
+    protected override async Task ApplyEffectsAsync(IEnumerable<ITarget> targets)
+    {
+        var damageEffect = new DamageEffect(DamageAmount)
+        {
+            Sound = Sound
+        };
+        
+        // todo: apply Exposed status
+        GD.Print("apply exposed");
+        
+        await damageEffect.ExecuteAllAsync(targets);
+    }
+}
---
+++ src/Game/Scripts/CustomResources/Cards/Warrior/WarriorTrueStrength.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CardGameV1.EffectSystem;
+using Godot;
+
+namespace CardGameV1.CustomResources.Cards.Warrior;
+
+[GlobalClass]
+public partial class WarriorTrueStrength : Card
+{
+    private const int MusclePerTurn = 2;
+    
+    protected override CardAttributes Attributes { get; } = new()
+    {
+        Id = nameof(WarriorTrueStrength),
+        Cost = 3,
+        Type = CardType.Power,
+        Rarity = CardRarity.Rare,
+        Target = CardTarget.Self,
+        TooltipText = $"[center]At the start of your turn, gain {MusclePerTurn} [color=\"ffdf00\"] Muscle[/color].[/center]",
+        IconPath = "res://art/tile_0127.png",
+        SoundPath = "res://art/true_strength.ogg",
+    };
+
+    protected override Task ApplyEffectsAsync(IEnumerable<ITarget> targets)
+    {
+        // todo: gain muscle status
+        GD.Print("gain muscle");
+        
+        return Task.CompletedTask;
+    }
+}
