4093f534cc1b1be833fb864865c5ce795f68a06f
syf <2654525303@qq.com>
2025-06-10 10:23:30 +0800

    create the BattleRewards scene
    - RewardButton scene

---
+++ src/Game/Scripts/UI/BattleReward/RewardButton.cs
@@ -0,0 +1,57 @@
+﻿using Godot;
+using GodotUtilities;
+
+namespace CardGameV1.UI.BattleReward;
+
+[Scene]
+public partial class RewardButton : Button
+{
+    [Export]
+    private Texture2D rewardIcon = null!;
+    [Export]
+    private string rewardText = null!;
+    
+    [Node]
+    private TextureRect customIcon = null!;
+    [Node]
+    private Label customText = null!;
+
+    public Texture2D RewardIcon
+    {
+        set => customIcon.Texture = value;
+    }
+
+    public string RewardText
+    {
+        set => customText.Text = value;
+    }
+
+    public override void _Ready()
+    {
+        RewardIcon = rewardIcon;
+        RewardText = rewardText;
+    }
+
+    public override void _EnterTree()
+    {
+        Pressed += OnPressed;
+    }
+
+    public override void _ExitTree()
+    {
+        Pressed -= OnPressed;
+    }
+
+    private void OnPressed()
+    {
+        QueueFree();
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+}
