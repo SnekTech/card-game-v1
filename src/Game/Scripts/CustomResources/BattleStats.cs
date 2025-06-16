6b0074d1de0c45035e4668b7a7778b5c1d53b7a2
syf <2654525303@qq.com>
2025-06-16 16:39:52 +0800

    create BattleStats records

---
+++ src/Game/Scripts/CustomResources/BattleStats.cs
@@ -0,0 +1,50 @@
+﻿using Godot;
+
+namespace CardGameV1.CustomResources;
+
+public record BattleStats
+{
+    public required int Tier { get; init; }
+    public required float Weight { get; init; }
+    public required (int min, int max) GoldReward { get; init; }
+    public required string EnemiesScenePath { get; init; }
+    
+    public float AccumulatedWeight { get; set; }
+
+    public int GoldRewardRoll => GD.RandRange(GoldReward.min, GoldReward.max);
+}
+
+public class BattleStatsPool
+{
+    private void Init()
+    {
+        var tier0Bats2 = new BattleStats
+        {
+            Tier = 0,
+            Weight = 2.5f,
+            GoldReward = (53,72),
+            EnemiesScenePath = "res://Scenes/battles/Tier0Bats2.tscn"
+        };
+        var tier0Crab = new BattleStats
+        {
+            Tier = 0,
+            Weight = 3f,
+            GoldReward = (49,68),
+            EnemiesScenePath = "res://Scenes/battles/Tier0Crab.tscn"
+        };
+        var tier1Bats3 = new BattleStats
+        {
+            Tier = 1,
+            Weight = 1f,
+            GoldReward = (63,81),
+            EnemiesScenePath = "res://Scenes/battles/Tier1Bats3.tscn"
+        };
+        var tier1BatCrab = new BattleStats
+        {
+            Tier = 1,
+            Weight = 2f,
+            GoldReward = (63,81),
+            EnemiesScenePath = "res://Scenes/battles/Tier1BatCrab.tscn"
+        };
+    }
+}
