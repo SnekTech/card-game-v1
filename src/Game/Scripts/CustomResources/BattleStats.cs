16455d4652fdc0d91e9eded0287d8ee2771227f6
syf <2654525303@qq.com>
2025-06-16 20:31:24 +0800

    create the BattleStatsPool

--- src/Game/Scripts/CustomResources/BattleStats.cs
+++ src/Game/Scripts/CustomResources/BattleStats.cs
@@ -1,4 +1,6 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Godot;
 
 namespace CardGameV1.CustomResources;
 
@@ -24,7 +26,48 @@ public class BattleStatsPool
         EnemiesScenePath = "res://Scenes/battles/Tier0Bats2.tscn"
     };
 
-    private void Init()
+    private BattleStatsPool()
+    {
+    }
+
+    private static BattleStatsPool? _instance;
+    public static BattleStatsPool Instance => _instance ??= new BattleStatsPool();
+
+    private readonly List<BattleStats> _pool = GenerateDefaultBattles();
+    private readonly float[] _totalWeightsByTier = [0, 0, 0];
+
+    private List<BattleStats> GetAllBattlesForTier(int tier) => _pool.Where(battle => battle.Tier == tier).ToList();
+
+    public void SetupWeightForTier(int tier)
+    {
+        var battles = GetAllBattlesForTier(tier);
+        _totalWeightsByTier[tier] = 0;
+
+        foreach (var battle in battles)
+        {
+            _totalWeightsByTier[tier] += battle.Weight;
+            battle.AccumulatedWeight = _totalWeightsByTier[tier];
+        }
+    }
+
+    public BattleStats GetRandomBattleForTier(int tier)
+    {
+        var roll = GD.RandRange(0, _totalWeightsByTier[tier]);
+        var battles = GetAllBattlesForTier(tier);
+
+        var rolledBattle = battles.FirstOrDefault(battle => battle.AccumulatedWeight > roll, battles[0]);
+        return rolledBattle;
+    }
+
+    public void Setup()
+    {
+        for (var i = 0; i < _totalWeightsByTier.Length; i++)
+        {
+            SetupWeightForTier(i);
+        }
+    }
+
+    private static List<BattleStats> GenerateDefaultBattles()
     {
         var tier0Bats2 = new BattleStats
         {
@@ -54,5 +97,7 @@ public class BattleStatsPool
             GoldReward = (63, 81),
             EnemiesScenePath = "res://Scenes/battles/Tier1BatCrab.tscn"
         };
+        var result = new List<BattleStats> { tier0Bats2, tier0Crab, tier1Bats3, tier1BatCrab };
+        return result;
     }
 }
