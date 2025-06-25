f83aeb9c0069ada6595c6593a547c342ab967ca6
syf <2654525303@qq.com>
2025-06-25 22:44:05 +0800

    use static ctor in SceneFactory
    - the initializer autoload is unnecessary

--- src/Game/Scripts/Autoload/SceneFactoryInitializer.cs
+++ src/Game/Scripts/Autoload/SceneFactoryInitializer.cs
@@ -10,21 +10,4 @@ using Godot;
 
 namespace CardGameV1.Autoload;
 
-public partial class SceneFactoryInitializer : Node
-{
-    public override void _Ready()
-    {
-        SceneFactory.Register<CardMenuUI>("res://Scenes/UI/card_pile_view/CardMenuUI.tscn");
-        SceneFactory.Register<Battle>("res://Scenes/Battle.tscn");
-        SceneFactory.Register<Enemy>("res://Scenes/Enemy/Enemy.tscn");
-        SceneFactory.Register<BattleRewardScene>("res://Scenes/UI/battle_reward/BattleReward.tscn");
-        SceneFactory.Register<CardRewards>("res://Scenes/UI/battle_reward/CardRewards.tscn");
-        SceneFactory.Register<RewardButton>("res://Scenes/UI/battle_reward/RewardButton.tscn");
-        SceneFactory.Register<CampfireScene>("res://Scenes/UI/campfire/Campfire.tscn");
-        SceneFactory.Register<ShopScene>("res://Scenes/UI/shop/Shop.tscn");
-        SceneFactory.Register<TreasureRoomScene>("res://Scenes/UI/treasure_room/TreasureRoom.tscn");
-        SceneFactory.Register<MapRoomScene>("res://Scenes/map/MapRoom.tscn");
-        SceneFactory.Register<MapLine>("res://Scenes/map/MapLine.tscn");
-        SceneFactory.Register<StatusUI>("res://Scenes/statusHandler/StatusUI.tscn");
-    }
-}
+public partial class SceneFactoryInitializer : Node;
--- src/Game/Scripts/SceneFactory.cs
+++ src/Game/Scripts/SceneFactory.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using CardGameV1.Character;
+using CardGameV1.Map;
+using CardGameV1.StatusSystem.UI;
+using CardGameV1.UI.BattleReward;
+using CardGameV1.UI.Campfire;
+using CardGameV1.UI.CardPileDisplay;
+using CardGameV1.UI.Shop;
+using CardGameV1.UI.TreasureRoom;
 using Godot;
 
 namespace CardGameV1;
 
 public static class SceneFactory
 {
+    static SceneFactory()
+    {
+        Register<CardMenuUI>("res://Scenes/UI/card_pile_view/CardMenuUI.tscn");
+        Register<Battle>("res://Scenes/Battle.tscn");
+        Register<Enemy>("res://Scenes/Enemy/Enemy.tscn");
+        Register<BattleRewardScene>("res://Scenes/UI/battle_reward/BattleReward.tscn");
+        Register<CardRewards>("res://Scenes/UI/battle_reward/CardRewards.tscn");
+        Register<RewardButton>("res://Scenes/UI/battle_reward/RewardButton.tscn");
+        Register<CampfireScene>("res://Scenes/UI/campfire/Campfire.tscn");
+        Register<ShopScene>("res://Scenes/UI/shop/Shop.tscn");
+        Register<TreasureRoomScene>("res://Scenes/UI/treasure_room/TreasureRoom.tscn");
+        Register<MapRoomScene>("res://Scenes/map/MapRoom.tscn");
+        Register<MapLine>("res://Scenes/map/MapLine.tscn");
+        Register<StatusUI>("res://Scenes/statusHandler/StatusUI.tscn");
+    }
+    
     private static readonly Dictionary<Type, string> Paths = [];
 
-    public static void Register<T>(string path) => Paths[typeof(T)] = path;
+    private static void Register<T>(string path) => Paths[typeof(T)] = path;
 
     public static T Instantiate<T>() where T : Node =>
         ResourceLoader.Load<PackedScene>(Paths[typeof(T)]).Instantiate<T>();
