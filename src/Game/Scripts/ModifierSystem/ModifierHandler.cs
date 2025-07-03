42d71dc7ea3c6b3b57c5c476d3a3b144501a2015
syf <2654525303@qq.com>
2025-07-03 15:49:36 +0800

    create ModifierHandler

---
+++ src/Game/Scripts/ModifierSystem/ModifierHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace CardGameV1.ModifierSystem;
+
+public class ModifierHandler
+{
+    private readonly List<Modifier> _modifiers = [];
+
+    public void AddModifier(Modifier modifier) => _modifiers.Add(modifier);
+
+    public bool HasModifier(ModifierType type) => _modifiers.Any(modifier => modifier.Type == type);
+
+    public int GetModifiedValue(int baseValue, ModifierType type)
+    {
+        var modifier = GetModifier(type);
+        return modifier?.GetModifiedValue(baseValue) ?? baseValue;
+    }
+    
+    private Modifier? GetModifier(ModifierType type) => _modifiers.FirstOrDefault(modifier => modifier.Type == type);
+}
