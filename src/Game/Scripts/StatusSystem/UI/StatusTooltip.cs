39025600388300bb97d7afe3fb055f54484adc59
syf <2654525303@qq.com>
2025-07-07 16:28:13 +0800

    create StatusTooltip scene

---
+++ src/Game/Scripts/StatusSystem/UI/StatusTooltip.cs
@@ -0,0 +1,26 @@
+﻿using GodotUtilities;
+
+namespace CardGameV1.StatusSystem.UI;
+
+[Scene]
+public partial class StatusTooltip : HBoxContainer
+{
+    [Node]
+    private TextureRect icon = null!;
+    [Node]
+    private Label label = null!;
+
+    public void Update(Status status)
+    {
+        icon.Texture = status.Icon;
+        label.Text = status.Tooltip;
+    }
+
+    public override void _Notification(int what)
+    {
+        if (what == NotificationSceneInstantiated)
+        {
+            WireNodes();
+        }
+    }
+}
