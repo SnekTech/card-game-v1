f3e2cb7f620acd07d62b31270e5f30c48623262d
syf <2654525303@qq.com>
2025-07-08 20:19:32 +0800

    create StatusView scene
    - add Cat-Lips Godot generators lib

---
+++ src/Game/Scripts/StatusSystem/UI/StatusView.cs
@@ -0,0 +1,58 @@
+﻿using CardGameV1.Constants;
+using CardGameV1.MyExtensions;
+
+namespace CardGameV1.StatusSystem.UI;
+
+[SceneTree]
+public partial class StatusView : Control
+{
+    public override void _Ready()
+    {
+        StatusTooltipContainer.ClearChildren();
+    }
+
+    public override void _EnterTree()
+    {
+        _.BackButton.Pressed += OnBackButtonPressed;
+    }
+
+    public override void _ExitTree()
+    {
+        _.BackButton.Pressed -= OnBackButtonPressed;
+    }
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent.IsActionPressed(InputActions.UICancel) && Visible)
+        {
+            HideView();
+        }
+    }
+
+    public override void _GuiInput(InputEvent inputEvent)
+    {
+        if (inputEvent.IsActionPressed(InputActions.LeftMouse) && Visible)
+        {
+            HideView();
+        }
+    }
+
+    public void ShowView(List<Status> statusList)
+    {
+        foreach (var status in statusList)
+        {
+            var newStatusTooltip = SceneFactory.Instantiate<StatusTooltip>();
+            StatusTooltipContainer.AddChild(newStatusTooltip);
+            newStatusTooltip.Status = status;
+        }
+        Show();
+    }
+
+    private void HideView()
+    {
+        StatusTooltipContainer.ClearChildren();
+        Hide();
+    }
+    
+    private void OnBackButtonPressed() => HideView();
+}
