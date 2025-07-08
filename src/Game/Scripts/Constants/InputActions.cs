a292eaff98c05b715c7e1c81a62bfed41937a8b0
syf <2654525303@qq.com>
2025-07-08 21:00:35 +0800

    use InputMap attribute to generate input actions

--- src/Game/Scripts/Constants/InputActions.cs
+++ src/Game/Scripts/Constants/InputActions.cs
@@ -1,9 +1,4 @@
 ﻿namespace CardGameV1.Constants;
 
-public static class InputActions
-{
-    public static readonly StringName LeftMouse = "left_mouse";
-    public static readonly StringName RightMouse = "right_mouse";
-    public static readonly StringName ScrollUp = "scroll_up";
-    public static readonly StringName ScrollDown = "scroll_down";
-}
+[InputMap]
+public static partial class InputActions;
