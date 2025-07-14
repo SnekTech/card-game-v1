4577073b79350e05ac88ec75ca28aaa40cac6b6c
syf <2654525303@qq.com>
2025-07-14 23:23:43 +0800

    fix rare card weight raising mechanic
    - also refactor CardRarityWeightStats.cs

--- src/Game/Scripts/CustomResources/Run/CardRarityWeightStats.cs
+++ src/Game/Scripts/CustomResources/Run/CardRarityWeightStats.cs
@@ -9,25 +9,31 @@ public class CardRarityWeightStats
     private const float BaseUncommonWeight = 3.7f;
     private const float BaseRareWeight = 0.3f;
 
-    private readonly Dictionary<CardRarity, WeightedCardRarity> _weightStats = new()
-    {
-        [CardRarity.Common] = new WeightedCardRarity
-            { Rarity = CardRarity.Common, Weight = BaseCommonWeight },
-        [CardRarity.Uncommon] = new WeightedCardRarity
-            { Rarity = CardRarity.Uncommon, Weight = BaseUncommonWeight },
-        [CardRarity.Rare] = new WeightedCardRarity
-            { Rarity = CardRarity.Rare, Weight = BaseRareWeight },
-    };
+    private readonly WeightedCardRarity _common = new() { Rarity = CardRarity.Common, Weight = BaseCommonWeight };
+    private readonly WeightedCardRarity _uncommon = new() { Rarity = CardRarity.Uncommon, Weight = BaseUncommonWeight };
+    private readonly WeightedCardRarity _rare = new() { Rarity = CardRarity.Rare, Weight = BaseRareWeight };
 
-    public CardRarity GetWeightedRarity() => _weightStats.Values.ToList().GetWeighted().Rarity;
+    public CardRarity GetWeightedRarity()
+    {
+        List<WeightedCardRarity> list = [_common, _uncommon, _rare];
+        return list.GetWeighted().Rarity;
+    }
 
     public void ModifyWeights(CardRarity rarityRolled)
     {
         if (rarityRolled == CardRarity.Rare)
         {
-            var rareWeight = _weightStats[CardRarity.Rare];
-            rareWeight.Weight = Mathf.Clamp(rareWeight.Weight + 0.3f, BaseRareWeight, 5f);
+            ResetRareWeight();
         }
+        else
+        {
+            RaiseSomeRareWeight();
+        }
+
+        return;
+
+        void RaiseSomeRareWeight() => _rare.Weight = Mathf.Clamp(_rare.Weight + 0.3f, BaseRareWeight, 5f);
+        void ResetRareWeight() => _rare.Weight = BaseRareWeight;
     }
 
     public record WeightedCardRarity : IWeightedCandidate
